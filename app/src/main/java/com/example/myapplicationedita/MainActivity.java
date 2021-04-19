5de61e9c2c04f8f55db5332957c3262205f8d2c2
EditaJur <82542281+EditaJur@users.noreply.github.com>
2021-04-19 20:28:31 +0300

    First button is set

--- app/src/main/java/com/example/myapplicationedita/MainActivity.java
+++ app/src/main/java/com/example/myapplicationedita/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.myapplicationedita;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void OnBtnclick(View view) {
+        this.tvMain.setText("Text was changed!");
     }
 }
