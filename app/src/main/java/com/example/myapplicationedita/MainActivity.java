1b470d3cfd5ee3b9479b159b8950c659277a738f
EditaJur <82542281+EditaJur@users.noreply.github.com>
2021-04-19 20:57:18 +0300

    Second button is set

--- app/src/main/java/com/example/myapplicationedita/MainActivity.java
+++ app/src/main/java/com/example/myapplicationedita/MainActivity.java
@@ -2,20 +2,31 @@ package com.example.myapplicationedita;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvMain;
+    Button button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.tvMain = findViewById(R.id.tvMain);
+        this. button2 = findViewById(R.id. button2);
     }
 
     public void OnBtnclick(View view) {
         this.tvMain.setText("Text was changed!");
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvMain.setTextColor(Color.YELLOW);
+            }
+        });
     }
 }
